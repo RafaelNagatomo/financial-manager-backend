297ffe7057062f70d2a70102e60e3dd4c11a5a18
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-06-30 15:25:51 -0300

    chore: add first, last name, email and password to User model

---
+++ prisma/migrations/20240630181740_add_first_last_name/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `first_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `last_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "first_name" TEXT NOT NULL,
+ADD COLUMN     "last_name" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
