234dc645468e5e9777ae435d4e1c689fc434265f
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-07-02 21:21:02 -0300

    feat: Installed and enabled cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import router from './routes/authRoutes';
 import transactionRoutes from './routes/transactionRoutes';
 import categoryRoutes from './routes/categoryRoutes';
@@ -7,6 +9,7 @@ import goalRoutes from './routes/goalRoutes';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/auth', router);
